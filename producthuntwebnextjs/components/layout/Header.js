8ab63329e1b1a125eaf939674bbfd4ee12448070
ManuelDevWeb <manueldeveloper17@gmail.com>
2021-11-13 01:27:52 -0500

    feat(style): Se agregaron estilos al header

--- producthuntwebnextjs/components/layout/Header.js
+++ producthuntwebnextjs/components/layout/Header.js
@@ -5,12 +5,48 @@ import Link from 'next/link';
 import Buscar from '../ui/Buscar';
 import Navegacion from './Navegacion';
 
+// Styles
+import styled from '@emotion/styled';
+import {css} from '@emotion/react';
+
+const ContenedorHeader = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+
+    @media (min-width: 768px) {
+        display: flex;
+        justify-content: space-between;
+    }
+`;
+
+const Logo= styled.p`
+    color: var(--naranja);
+    font-size: 4rem; // 40px
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+`;
+
+
+
+
 const Header = () => {
     return (  
-        <header>
-            <div>
+        <header
+            // Estilos header
+            css={css`
+                border-bottom: 2px solid var(--gris3);
+                padding: 1rem 0;
+            `}
+        >
+            <ContenedorHeader>
                 <div>
-                    <p>P</p>
+                    <Link href="/">
+                        <Logo>P</Logo>
+                    </Link>
+                    
                     <Buscar/>
                     <Navegacion />
                 </div>
@@ -20,7 +56,7 @@ const Header = () => {
                     <Link href="/">Login</Link>
                     <Link href="/">Crear Cuenta</Link>
                 </div>
-            </div>
+            </ContenedorHeader>
         </header>
     );
 }
