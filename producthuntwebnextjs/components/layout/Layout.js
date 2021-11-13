f6be3038da2614376dea91b51f29919fc78d15eb
ManuelDevWeb <manueldeveloper17@gmail.com>
2021-11-13 01:18:28 -0500

    feat(ci): Se agrego la configuración del head y algunos estilos en el componente Layout

--- producthuntwebnextjs/components/layout/Layout.js
+++ producthuntwebnextjs/components/layout/Layout.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Head from "next/head";
+
+// Styles
+import { Global, css } from "@emotion/react";
 
 // Components
 import Header from "./Header";
@@ -6,7 +10,71 @@ import Header from "./Header";
 const Layout = (props) => {
   return (
     <>
-      <Header/>
+      {/* Estilos globales que se aplican en toda la aplicación */}
+      <Global
+        styles={css`
+          :root {
+            --gris: #3d3d3d;
+            --gris2: #6f6f6f;
+            --naranja: #da552f;
+          }
+
+          html {
+            font-size: 62.5%;
+            box-sizing: border-box;
+          }
+          *,
+          *:before,
+          *:after {
+            box-sizing: inherit;
+          }
+
+          body {
+            font-size: 1.6rem; // 16px
+            line-height: 1.5;
+            font-family: 'PT Sans', sans-serif;
+          }
+
+          h1,
+          h2,
+          h3 {
+            margin: 0 0 2rem 0;
+            line-height: 1.5;
+          }
+
+          h1,h2{
+            font-family: 'Roboto Slab', serif;
+            font-weight: 700;
+          }
+
+          h3{
+            font-family: 'PT Sans', sans-serif;
+          }
+
+          ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+          }
+
+          a {
+            text-decoration: none;
+          }
+        `}
+      />
+
+      {/* Información Meta y configuración de nuestra página */}
+      <Head>
+        <html lang="es"/>
+        <title>Product Hunt Firebase & Next.js</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="/static/css/app.css" />
+      </Head>
+
+      <Header />
       <main>{props.children}</main>
     </>
   );
