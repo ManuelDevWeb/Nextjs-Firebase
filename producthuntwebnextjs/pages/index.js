dae9a732225baaa3e50b7d023f6402b686859e4e
ManuelDevWeb <manueldeveloper17@gmail.com>
2021-11-24 23:34:50 -0500

    feat: Se empezo a trabajar en el componente index

--- producthuntwebnextjs/pages/index.js
+++ producthuntwebnextjs/pages/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import styled from '@emotion/styled';
+import React, {useEffect, useState, useContext} from 'react';
+
+// Context
+import firebase from '../firebase';
 
 // Components
 import Layout from '../components/layout/Layout';
@@ -8,6 +10,36 @@ import Layout from '../components/layout/Layout';
 
 
 const  Home=()=> {
+  // State de los productos
+  const [productos, setProductos] = useState([]);
+
+  /*
+    Podemos manejar extraer el Context firebase de la siguiente forma, 
+    (Se debe importar {FirebaseContext} from '../firebase/index')
+     const { firebase } = useContext(FirebaseContext);
+  */
+
+  // UseEffect para obtener los productos
+  useEffect(()=>{
+    const getProducts=()=>{
+      firebase.db.collection('productos').orderBy('creado', 'desc').onSnapshot(handleSnapshot);
+    }
+    getProducts();
+  },[])
+
+  // Función para manejar el snapshot que contiene la información de los productos de la BD
+  function handleSnapshot(snapshot){
+    // Creamos el array de objetos con cada uno de los productos del snapshot
+    const productos=snapshot.docs.map(doc=>(
+      {
+        id: doc.id,
+        ...doc.data()
+      }
+    ));
+    // console.log(products);
+    setProductos(productos);
+  }
+
   return (
     <div>
       <Layout>
