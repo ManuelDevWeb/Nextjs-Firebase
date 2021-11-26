34dd051a1a5a15b1008faa7967f2e346c322e302
ManuelDevWeb <manueldeveloper17@gmail.com>
2021-11-25 21:53:34 -0500

    feat: Se agrego la funcionalidad para validar que un producto exista, en las rutas dinámicas

--- producthuntwebnextjs/pages/productos/[id].js
+++ producthuntwebnextjs/pages/productos/[id].js
@@ -1,23 +1,65 @@
-import React,{useEffect} from 'react';
-import {useRouter} from 'next/router';
+import React, { useEffect, useContext, useState } from "react";
+import { useRouter } from "next/router";
 
-const Producto = (props) => {
-    // Routing para obtener el id actual
-    const router = useRouter();
-    //console.log(router);
-    const {query: {id}} = router;
+// Context
+/*
+    También, podemos manejar extraer el Context firebase de la siguiente forma, 
+    (Se debe importar firebase from '../firebase')     
+*/
+import { FirebaseContext } from "../../firebase";
 
-    useEffect(() => {
-        // Validamos que si venga un id por la url
-        if(id){
-            console.log(id);
+// Componentes
+import Layout from "../../components/layout/Layout";
+import Error404 from "../../components/layout/404";
+
+const Producto = () => {
+  // State para manejar el producto
+  const [producto, setProducto] = useState({});
+  // State para manejar el error
+  const [error, setError] = useState(false);
+
+  // Context de firebase
+  const { firebase } = useContext(FirebaseContext);
+
+  // Routing para obtener el id actual
+  const router = useRouter();
+  //console.log(router);
+  const {
+    query: { id },
+  } = router;
+
+  useEffect(() => {
+    // Validamos que si venga un id por la url
+    if (id) {
+      const obtenerProducto = async () => {
+        // Obtenemos por id, el producto de firebase
+        const productoQuery = await firebase.db.collection("productos").doc(id);
+        // Almacenamos el producto en una variable para enviarlo al estado
+        const producto = await productoQuery.get();
+        // console.log(producto.data());
+
+        // Validando que exista el producto
+        if (producto.exists) {
+          // Almacenamos el producto en el state
+          setProducto(producto.data());
+        } else {
+          setError(true);
         }
-    }, [id]);
+      };
+      obtenerProducto();
+    }
+  }, [id]);
 
+  return (
+    <Layout>
+      <>
+        {
+          // Si error es verdadero retornamos componente Error404
+          error && <Error404 /> 
+        }        
+      </>
+    </Layout>
+  );
+};
 
-    return (
-        <h1>Desde {id}</h1>
-    );
-}
- 
 export default Producto;
